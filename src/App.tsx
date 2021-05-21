6c875281386f3dd913fb2e82e78bc787cf632c52
makotot <makoto.ttn@gmail.com>
2021-05-22 02:25:07 +0900

    add checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import useInterval from '@use-it/interval';
 
 const Grid = styled('div', {
   display: 'grid',
-  gridTemplateColumns: '1fr 1fr 1fr',
+  gridTemplateColumns: 'minmax(120px, 240px) 1fr 1fr 1fr',
   columnGap: '1rem',
   height: '100vh',
   overflow: 'hidden',
@@ -45,8 +45,19 @@ const globalStyle = global({
   },
 });
 
+const Setting = styled('div', {
+  padding: '1rem',
+  overflow: 'scroll',
+  wordBreak: 'break-all',
+});
+
 function App() {
   globalStyle();
+
+  const [value, updateValue] = useState('# ReactMarkdown Playground');
+  const handleUpdateValue = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    updateValue(e.target.value);
+  };
 
   const json = useRef({});
   const log = () => {
@@ -54,24 +65,46 @@ function App() {
       json.current = tree;
     };
   };
-  const [value, updateValue] = useState('# ReactMarkdown Playground');
-  const handleUpdateValue = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    updateValue(e.target.value);
-  };
-
   const [jsonTree, updateJsonTree] = useState({});
   useInterval(() => {
     updateJsonTree(json.current);
   }, 1000);
 
+  const pluginMap = {
+    log,
+    gfm,
+  };
+  const [remarkPlugins, updateRemarkPlugins] = useState<string[]>(['log']);
+  const handleUpdateRemarkPlugins = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (remarkPlugins.includes(value)) {
+      updateRemarkPlugins(remarkPlugins.filter((plugin) => plugin !== value));
+    } else {
+      updateRemarkPlugins([...remarkPlugins, value]);
+    }
+  };
+
   return (
     <div className="App">
       <Grid>
+        <Setting>
+          <input
+            type="checkbox"
+            value="gfm"
+            onChange={handleUpdateRemarkPlugins}
+          />
+          <label>gfm</label>
+        </Setting>
         <Editor>
           <Textarea value={value} onChange={handleUpdateValue} />
         </Editor>
         <Preview>
-          <ReactMarkdown remarkPlugins={[gfm, log]}>{value}</ReactMarkdown>
+          <ReactMarkdown
+            remarkPlugins={Object.entries(pluginMap)
+              .filter((entry) => remarkPlugins.includes(entry[0]))
+              .map((entry) => entry[1])}>
+            {value}
+          </ReactMarkdown>
         </Preview>
         <JsonView>
           <ReactJson src={jsonTree} collapsed={3} />
